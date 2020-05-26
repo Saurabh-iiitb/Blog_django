ecebe610bd699c8c14d1a4b341694410e8e716d4
Ayush Mishra <mishraayush400@gmail.com>
2020-05-26 13:26:24 +0530

    Update tests.py

--- src/posts/tests.py
+++ src/posts/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import Post, Author, Category
 
 # Create your tests here.
+
+class CategoryModelTest(TestCase):
+	@classmethod
+	def setUpTestData(cls):
+		Category.objects.create(title='firstPost') 
+
+	def test_title_label(self):
+		post = Category.objects.get(id=1)
+		field_label = post._meta.get_field('title').verbose_name
+		self.assertEquals(field_label, 'title')
+
+	def test_first_name_max_length(self):
+		author = Category.objects.get(id=1)
+		max_length = author._meta.get_field('title').max_length
+		self.assertEquals(max_length, 20)
